f027ad253e3f90ad01245a5156e9dd647a006496
pihuu <ansuman.diptioy@outlook.com>
2020-07-19 23:59:05 +0530

    sample

---
+++ config.js
@@ -0,0 +1,7 @@
+export const testUrl = 'https://www.ebay.com/'
+export const firstName = 'ansuman'
+export const lastName = 'Testing'
+export const email = 'ansuman@qa.com'
+export const password = 'XXXX1234@'
+
+export const time = 6000
---
+++ tests/sample.test.js
@@ -0,0 +1,7 @@
+describe ('Sample Test Case to delete', async function() {
+
+    it('Sample One', async function() {
+
+        await page.goto('https://www.google.com')
+    })
+})
